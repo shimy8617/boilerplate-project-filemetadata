8cf8fe0ea9a08c7fa97948c51f6355540d8dea7a
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2022-01-05 10:58:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var bodyParser = require("body-parser");
 
 var app = express();
+
+var cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect = process.env.MONGO_URI;
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +26,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  })  
+});
 
 
 const port = process.env.PORT || 3000;
